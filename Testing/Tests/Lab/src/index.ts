d528db3a7f060dd2259b36906c0882ca2228972e
KristianFJones <me@kristianjones.dev>
2020-03-28 15:45:58 +0000

    tests: Add Assertions to tests.

--- Testing/Tests/Lab/src/index.ts
+++ Testing/Tests/Lab/src/index.ts
@@ -1,14 +1,15 @@
 // Testing/Tests/Lab/src/index.ts
 import { add, divide } from './Utils/Math';
+import { equal } from 'assert';
 
 export async function startApp(): Promise<void> {
   console.debug('Starting Application');
 
   const sum = add(1, 1);
-  if (sum !== 2) throw new Error(`add(1, 1) !== 2`);
+  equal(sum, 2);
 
   const divideResult = divide(2, 2);
-  if (divideResult !== 1) throw new Error(`divide(2, 2) !== 1`);
+  equal(divideResult, 1);
 
   console.debug('Done');
 }
--- Testing/Tests/React-SSR/src/index.ts
+++ Testing/Tests/React-SSR/src/index.ts
@@ -1,8 +1,14 @@
 // Template/src/index.ts
 import { renderUI } from './Server';
+import { match } from 'assert';
+
+const htmlRenderedString =
+  '<h1>Hello World</h1><p>I&#x27;m an example application</p><h1>Home</h1><button class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary" tabindex="0" type="button"><span class="MuiButton-label">Primary</span></button>';
 
 async function ssrServer(): Promise<void> {
   const htmlString = await renderUI();
+
+  match(htmlString, new RegExp(htmlRenderedString));
 
   console.log(`HTML String to send to client: ${htmlString}`);
 }
