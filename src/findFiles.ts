6167dc05261a72b5d2ce531664be7b34768b30cd
Kristian Jones <me@kristianjones.dev>
2020-03-28 11:49:24 -0500

    fix(findFiles): Actually properly implement Node resolution by allowing import of directories with `index.{js.jsx,ts,tsx}` (#24)

--- src/findFiles.ts
+++ src/findFiles.ts
@@ -10,34 +10,54 @@ interface FileRule {
   extensions: string[];
 }
 
-async function findFileReadDir(
+async function findFile(
   cwd: string,
   { fileName, extensions }: FileRule,
 ): Promise<string | undefined> {
   // Get all files in a directory
-  const directoryFiles = await fs.readdir(cwd);
+  const directoryFiles = await fs.readdir(cwd, { withFileTypes: true });
 
   // Filter the diretory files to only thoose with passed extenison and `.js` or `.jsx`
-  const matchedFiles = directoryFiles.filter((directoryFileName) => {
-    for (let extension of [...extensions, ...JS_EXTS]) {
-      if (directoryFileName === fileName + extension) {
-        return true;
+  const matchedFiles = directoryFiles.filter((directoryEntry) => {
+    const directoryFileName = directoryEntry.name;
+
+    if (directoryEntry.name.includes(fileName)) {
+      if (directoryEntry.isDirectory()) return true;
+
+      for (let extension of [...extensions, ...JS_EXTS]) {
+        if (directoryFileName === fileName + extension) {
+          return true;
+        }
       }
     }
 
     return false;
   });
 
-  if (matchedFiles.length > 1 || matchedFiles.length < 1) return undefined;
+  let matchedFileName: string;
+  if (matchedFiles.length > 1 || matchedFiles.length < 1) {
+    return undefined;
+  } else {
+    const matchedEntry = matchedFiles[0];
 
-  return resolvePath(cwd, matchedFiles[0]);
+    if (matchedEntry.isDirectory()) {
+      return findFile(resolvePath(cwd, matchedEntry.name), {
+        fileName: 'index',
+        extensions,
+      });
+    }
+
+    matchedFileName = matchedEntry.name;
+  }
+
+  return resolvePath(cwd, matchedFileName);
 }
 
 export async function findFiles(
   cwd: string,
   fileRules: FileRule,
 ): Promise<URL> {
-  const filePath = await findFileReadDir(cwd, fileRules);
+  const filePath = await findFile(cwd, fileRules);
   if (!filePath) throw new Error('No files found by finder');
 
   return pathToFileURL(filePath);
