69b79836ea2b04a83e395d79569c81dbd01b03f0
Kristian Jones <me@kristianjones.dev>
2020-05-12 17:37:09 -0500

    Revert "fix(dot import): Fix `import XYZ from '.'` (#52)"
    This reverts commit b4194c0072350aabf9db7aab442c97b44ce47a85.

--- Testing/Tests/DotImport/src/Hello/helloWorld.ts
+++
@@ -1,5 +0,0 @@
-// DotImport/src/Hello/helloWorld.ts
-import { helloString } from '.';
-import { deepEqual } from 'assert';
-
-deepEqual(helloString, 'helloWorld', 'helloString does not equal helloWorld');
