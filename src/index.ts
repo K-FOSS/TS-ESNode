8f3f83313fad3426075c10b5ea0edbaac835936f
KristianFJones <me@kristianjones.dev>
2020-05-25 23:24:02 +0000

    Revert "ci(index.ts): Purposefuly break transformSource's context type to test TSC Job"
    This reverts commit 5c7dbf3e055196834c340303295edf01d4d629ca.

--- src/index.ts
+++ src/index.ts
@@ -149,7 +149,7 @@ export async function getFormat(
 
 export async function transformSource(
   source: Source,
-  context: string,
+  context: TransformContext,
   defaultTransformSource: Function,
 ): Promise<TransformResponse> {
   const resolvedUrl = new URL(context.url);
