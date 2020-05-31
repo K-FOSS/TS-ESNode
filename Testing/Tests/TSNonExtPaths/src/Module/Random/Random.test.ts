c9e35623424bfee32ce0fc07f9296e385104dddc
KristianFJones <me@kristianjones.dev>
2020-05-31 18:37:05 +0000

    tests(TSNonExtPaths): More testing on the TSNonExtPaths Suite

---
+++ Testing/Tests/TSNonExtPaths/src/Module/Random/Random.test.ts
@@ -0,0 +1,29 @@
+// Testing/Tests/TSNonExtPaths/src/Module/Random/Random.test.ts
+import { TestSuite } from '@k-foss/ts-estests';
+import { deepStrictEqual, strictEqual } from 'assert';
+
+export class TSNonExtPathRandomSuite extends TestSuite {
+  public testName = 'TSNonExtPaths - Modules/Random/index.ts Suite';
+
+  public async test(): Promise<void> {
+    const [{ sayRandom }, importRelative, importDot] = await Promise.all([
+      import('random'),
+      import('./'),
+      import('.'),
+    ]);
+
+    deepStrictEqual(importRelative, importDot, 'importRelative === importDot');
+
+    deepStrictEqual(
+      sayRandom(),
+      await importRelative.randomModule(),
+      'sayRandom() === importRelative.randomModule()',
+    );
+
+    deepStrictEqual(
+      await importRelative.randomModule(),
+      await importDot.randomModule(),
+      'importRelative.randomModule() === importDot.randomModule()',
+    );
+  }
+}
