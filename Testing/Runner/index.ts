ac932af48812524fd1cd60dd383e05b175a67639
KristianFJones <me@kristianjones.dev>
2020-05-31 16:32:59 +0000

    tests: Fix tests

--- Testing/Runner/index.ts
+++ Testing/Runner/index.ts
@@ -6,7 +6,9 @@ import { promises as fs } from 'fs';
 async function runTests(): Promise<void> {
   await installTestsDependencies();
 
-  await fs.symlink(process.cwd(), 'node_modules/@k-foss/ts-esnode', 'dir');
+  try {
+    await fs.symlink(process.cwd(), 'node_modules/@k-foss/ts-esnode', 'dir');
+  } catch {}
 
   await run('npx ts-estest ./Testing/Tests', {
     cwd: process.cwd(),
